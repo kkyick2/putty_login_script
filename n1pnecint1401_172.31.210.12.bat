1903d904a361735ea1e152ac7792717c4d30e9cc
kkyick2 <kkyick2@gmail.com>
2025-05-22 10:56:33 +0800

    20250522 1056

--- n1pnecint1401_172.31.210.12.bat
+++ n1pnecint1401_172.31.210.12.bat
@@ -1,7 +1,7 @@
 @echo off
 SET IPADDR=172.31.210.12
 SET PORT=22
-SET CREDFILE=cred.txt
+SET CREDFILE=cred_empf.txt
 
 for /F "tokens=1,2 delims=," %%a in (%CREDFILE%) do ( 
  start putty.exe -ssh %%a@%IPADDR% -pw %%b -P %PORT%
